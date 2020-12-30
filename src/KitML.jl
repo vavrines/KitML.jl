28e7bbdd95a2e3c0cf3312dfe75e910cc69914ee
Tianbai Xiao <tianbaixiao@gmail.com>
2020-12-30 19:40:23 +0100

    Update test

--- src/KitML.jl
+++ src/KitML.jl
@@ -1,8 +1,8 @@
-# ============================================================
-# Kinetic.jl : The lightweight module with neural 
-#              differential equations of Kinetic.jl
-# Copyright (c) 2020 Tianbai Xiao <tianbaixiao@gmail.com>
-# ============================================================
+"""
+KitML.jl : The lightweight module of neural differential equations within Kinetic.jl
+Copyright (c) 2020 Tianbai Xiao <tianbaixiao@gmail.com>
+
+"""
 
 module KitML
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -20,4 +20,6 @@ fh = zeros(21)
 nn2 = Chain(Dense(42, 42, tanh), Dense(42, 42, tanh))
 p2 = initial_params(nn2)
 
+KitML.step_ube!(fw, fh, fh, w, prim, f, f, fw, fh, fh, u, ω, (2.0, 5/3, 0.01, 0.5, 1.0, 0.1, 0.001, zeros(3), zeros(3), nn2, p2); mode=:bgk)
+KitML.step_ube!(fw, fh, fh, w, prim, f, f, fw, fh, fh, u, ω, (2.0, 5/3, 0.01, 0.5, 1.0, 0.1, 0.001, zeros(3), zeros(3), nn2, p2); mode=:shakhov)
 KitML.step_ube!(fw, fh, fh, w, prim, f, f, fw, fh, fh, u, ω, (2.0, 5/3, 0.01, 0.5, 1.0, 0.1, 0.001, zeros(3), zeros(3), nn2, p2); mode=:nn)
