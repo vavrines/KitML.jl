629ced258719b58c332b45484815215f93d5f25d
Tianbai Xiao <tianbaixiao@gmail.com>
2021-01-26 16:50:29 +0100

    Add GPU training support

--- src/Train/train.jl
+++ src/Train/train.jl
@@ -37,8 +37,8 @@ end
 
 
 """
-    sci_train!(ann, data, opt = ADAM(), ne = 1, nb = 256)
-    sci_train!(ann, dl::Flux.Data.DataLoader, opt = ADAM(), ne = 1)
+    sci_train!(ann, data, opt = ADAM(), ne = 1, nb = 256; device = cpu)
+    sci_train!(ann, dl::Flux.Data.DataLoader, opt = ADAM(), ne = 1; device = cpu)
 
 Scientific machine learning training function
 
@@ -47,17 +47,22 @@ Scientific machine learning training function
 - @args opt: optimizer 
 - @args ne: epoch number
 - @args nb: batch size
+- @args device: cpu / gpu
 """
-function sci_train!(ann, data::Tuple, opt = ADAM(), ne = 1, nb = 256)
-    L = size(data[1], 2)
-    loss(x, y) = sum(abs2, ann(x) - y) / L
+function sci_train!(ann, data::Tuple, opt = ADAM(), ne = 1, nb = 256; device = cpu)
+    X, Y = data |> device
+    L = size(X, 2)
+    data = Flux.Data.DataLoader(X, Y, batchsize = nb, shuffle = true) |> device
+    
+    ann = device(ann)
     ps = params(ann)
-    cb = () -> println("loss: $(loss(data[1], data[2]))")
+    loss(x, y) = sum(abs2, ann(x) - y) / L
+    cb = () -> println("loss: $(loss(X, Y))")
 
     Flux.@epochs ne Flux.train!(
         loss,
         ps,
-        Flux.Data.DataLoader(data[1], data[2], batchsize = nb, shuffle = true),
+        data,
         opt,
         cb = Flux.throttle(cb, 1),
     )
@@ -65,11 +70,15 @@ function sci_train!(ann, data::Tuple, opt = ADAM(), ne = 1, nb = 256)
     return nothing
 end
 
-function sci_train!(ann, dl::Flux.Data.DataLoader, opt = ADAM(), ne = 1)
-    L = size(dl.data[1], 2)
-    loss(x, y) = sum(abs2, ann(x) - y) / L
+function sci_train!(ann, dl::Flux.Data.DataLoader, opt = ADAM(), ne = 1; device = cpu)
+    X, Y = dl.data |> device
+    L = size(X, 2)
+    dl = dl |> device
+    
+    ann = device(ann)
     ps = params(ann)
-    cb = () -> println("loss: $(loss(dl.data[1], dl.data[2]))")
+    loss(x, y) = sum(abs2, ann(x) - y) / L
+    cb = () -> println("loss: $(loss(X, Y))")
 
     Flux.@epochs ne Flux.train!(
         loss,
