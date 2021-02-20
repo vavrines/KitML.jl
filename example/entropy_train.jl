ca49157b1581c61cbaede7e372ca7e6d1d8e94c5
vavrines <tianbaixiao@gmail.com>
2021-02-20 11:19:47 +0100

    Update entropy_train.jl

--- example/entropy_train.jl
+++ example/entropy_train.jl
@@ -9,8 +9,19 @@ Y = data.h |> permutedims
 
 data = nothing # deallocate
 
-nn = KitML.ICNNChain(4, 1, [16, 64, 64, 8], tanh)
-KitML.sci_train!(nn, (X, Y), ADAM(), 2, 256)
+nn = KitML.ICNNChain(4, 1, [16, 32, 32, 8], tanh)
+KitML.sci_train!(nn, (X, Y), ADAM(); epoch = 1, batch = 4000, device = cpu)
 
-fnn = KitML.FastICNN(4, 1, [16, 64, 64, 8], tanh)
-res = KitML.sci_train(fnn, (X, Y); device = cpu)
+loss(x, y) = sum(abs2, nn(x) - y) / 15968000
+
+for iter = 1:1000
+    KitML.sci_train!(nn, (X, Y), ADAM(); epoch = 5, batch = 4000, device = cpu)
+
+    if iter % 100 == 0
+        @show loss(X, Y)
+        KitML.save_model(nn)
+    end
+end
+
+#fnn = KitML.FastICNN(4, 1, [16, 64, 64, 8], tanh)
+#res = KitML.sci_train(fnn, (X, Y); device = cpu, maxiters = 200)
