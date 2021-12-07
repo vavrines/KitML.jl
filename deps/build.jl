38d7eb3ab64d259d0f9808d6718f5a028a89667d
Tianbai Xiao <tianbaixiao@gmail.com>
2021-12-07 22:30:08 +0100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,10 @@
 using KitBase.PyCall
 
-try
-    using Conda
-    Conda.add_channel("conda-forge")
-    Conda.add("tensorflow")
-    tf = pyimport("tensorflow")
-catch
-    cmd = `pip3 install tensorflow --user`
-    run(cmd)
-    tf = pyimport("tensorflow")
-end
+#using Conda
+#Conda.add_channel("conda-forge")
+#Conda.add("tensorflow")
+#tf = pyimport("tensorflow")
+
+cmd = `pip3 install tensorflow --user`
+run(cmd)
+#tf = pyimport("tensorflow")
