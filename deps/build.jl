2c3f91263970c3f2f27e03856bb2be12a8df0158
vavrines <tianbaixiao@gmail.com>
2021-12-17 17:06:38 +0100

    Change minor issues

--- deps/build.jl
+++ deps/build.jl
@@ -3,8 +3,8 @@ using KitBase.PyCall
 #using Conda
 #Conda.add_channel("conda-forge")
 #Conda.add("tensorflow")
-#tf = pyimport("tensorflow")
 
 cmd = `pip3 install tensorflow --user`
 run(cmd)
+
 #tf = pyimport("tensorflow")
