435e04417633fba86c7a85fd50afc38b33b95df9
vavrines <tianbaixiao@gmail.com>
2021-12-17 17:04:49 +0100

    Add 3d regime data generator

--- src/Data/data.jl
+++ src/Data/data.jl
@@ -12,6 +12,8 @@ function regime_data(args...)
         regime_data_1d(args...)
     elseif length(args[1]) == 4
         regime_data_2d(args...)
+    elseif length(args[1]) == 5
+        regime_data_3d(args...)
     end
 end
 
@@ -20,6 +22,8 @@ function regime_data(ks::SolverSet, args...)
         regime_data_1d(ks, args...)
     elseif length(args[1]) == 4
         regime_data_2d(ks, args...)
+    elseif length(args[1]) == 5
+        regime_data_3d(ks, args...)
     end
 end
 
@@ -63,3 +67,26 @@ function regime_data_2d(w, swx, swy, f, u, v, K::Real, Kn::Real, μ = ref_vhs_vi
 end
 
 regime_data_2d(ks::SolverSet, w, swx, swy, f) = regime_data(w, swx, swy, f, ks.vs.u, ks.vs.v, ks.gas.K, ks.gas.Kn)
+
+function regime_data_3d(cons, swx, swy, swz, f, u, v, w, K::Real, Kn::Real, μ = ref_vhs_vis(Kn, 1.0, 0.5), ω = 0.81)
+    γ = heat_capacity_ratio(K, 3)
+    prim = conserve_prim(cons, γ)
+    
+    Mu, Mv, Mw, _, _1 = gauss_moments(prim, K)
+    a = pdf_slope(prim, swx, K)
+    b = pdf_slope(prim, swy, K)
+    c = pdf_slope(prim, swz, K)
+    swt = -prim[1] .* (moments_conserve_slope(a, Mu, Mv, Mw, 1, 0, 0) .+ moments_conserve_slope(b, Mu, Mv, Mw, 0, 1, 0) .+ moments_conserve_slope(c, Mu, Mv, Mw, 0, 0, 1))
+    A = pdf_slope(prim, swt, K)
+    tau = vhs_collision_time(prim, μ, ω)
+    fr = chapman_enskog(u, v, w, prim, a, b, c, A, tau)
+    L = norm((f .- fr) ./ prim[1])
+
+    sw = (swx.^2 + swy.^2 + swz.^2).^0.5
+    x = [cons; sw; tau]
+    y = ifelse(L <= 0.005, 0.0, 1.0)
+
+    return x, y
+end
+
+regime_data_3d(ks::SolverSet, w, swx, swy, swz, f) = regime_data(w, swx, swy, swz, f, ks.vs.u, ks.vs.v, ks.vs.w, ks.gas.K, ks.gas.Kn)
--- test/test_data.jl
+++ test/test_data.jl
@@ -4,20 +4,12 @@ using KitML.KitBase
 begin
     set = Setup(space = "1d1f1v")
     ps = PSpace1D(0.0, 1.0, 10)
-    vs = VSpace1D(-5.0, 5.0, 24)
+    vs = VSpace1D(-5.0, 5.0, 16)
     gas = Gas(K = 0.0)
     
     prim0 = [1.0, 0.0, 1.0]
     fw = (args...) -> prim_conserve(prim0, gas.γ)
-    ff = function(args...)
-        prim = conserve_prim(fw(args...), gas.γ)
-        h = maxwellian(vs.u, vs.v, prim)
-        return h
-    end
-    bc = function(x, y)
-        return prim0
-    end
-    ib = IB1F(fw, ff, bc)
+    ib = IB1F(fw, vs, gas)
 
     ks = SolverSet(set, ps, vs, gas, ib)
 end
@@ -27,37 +19,17 @@ prim = conserve_prim(w, 3.0)
 f = maxwellian(ks.vs.u, prim)
 regime_data(ks, w, zeros(3), f)
 
+
 #--- 2D ---#
 begin
-    set = Setup(
-        case = "cylinder",
-        space = "2d2f2v",
-        boundary = ["maxwell", "extra", "mirror", "mirror"],
-        limiter = "minmod",
-        cfl = 0.5,
-        maxTime = 15.0, # time
-    )
-    ps = CSpace2D(1.0, 6.0, 10, 0.0, π, 10, 1, 1)
+    set = Setup(space = "2d2f2v")
+    ps = PSpace1D(0.0, 1.0, 10)
     vs = VSpace2D(-10.0, 10.0, 24, -10.0, 10.0, 24)
-    gas = Gas(Kn = 1e-2, Ma = 5.0, K = 1.0)
+    gas = Gas(K = 1.0)
     
     prim0 = [1.0, 0.0, 0.0, 1.0]
-    prim1 = [1.0, gas.Ma * sound_speed(1.0, gas.γ), 0.0, 1.0]
-    fw = (args...) -> prim_conserve(prim1, gas.γ)
-    ff = function(args...)
-        prim = conserve_prim(fw(args...), gas.γ)
-        h = maxwellian(vs.u, vs.v, prim)
-        b = h .* gas.K / 2 / prim[end]
-        return h, b
-    end
-    bc = function(x, y)
-        if abs(x^2 + y^2 - 1) < 1e-3
-            return prim0
-        else
-            return prim1
-        end
-    end
-    ib = IB2F(fw, ff, bc)
+    fw = (args...) -> prim_conserve(prim0, gas.γ)
+    ib = IB2F(fw, vs, gas)
 
     ks = SolverSet(set, ps, vs, gas, ib)
 end
@@ -66,3 +38,22 @@ w = [1.0, 0.0, 0.0, 1.0]
 prim = conserve_prim(w, 5/3)
 f = maxwellian(ks.vs.u, ks.vs.v, prim)
 regime_data(ks, w, zeros(4), zeros(4), f)
+
+#--- 3D ---#
+begin
+    set = Setup(space = "1d1f3v")
+    ps = PSpace1D(0.0, 1.0, 10)
+    vs = VSpace3D(-10, 10, 16, -10, 10, 16, -10, 10, 16)
+    gas = Gas(K = 0.0)
+    
+    prim0 = [1.0, 0.0, 0.0, 1.0]
+    fw = (args...) -> prim_conserve(prim0, gas.γ)
+    ib = IB1F(fw, vs, gas)
+
+    ks = SolverSet(set, ps, vs, gas, ib)
+end
+
+w = [1.0, 0.0, 0.0, 0.0, 1.0]
+prim = conserve_prim(w, 5/3)
+f = maxwellian(ks.vs.u, ks.vs.v, ks.vs.w, prim)
+regime_data(ks, w, zeros(5), zeros(5), zeros(5), f)
