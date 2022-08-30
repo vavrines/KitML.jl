bfe5d740d747d51eac160f88d906e639643ed392
vavrines <tianbaixiao@gmail.com>
2022-08-30 16:25:05 +0200

    Extend bgknet for reduced distributions

--- src/Layer/layer.jl
+++ src/Layer/layer.jl
@@ -24,15 +24,33 @@ struct BGKNet{T1,T2}
     νnet::T2
 end
 
-(nn::BGKNet)(x, p, vs = VSpace1D(-6, 6, size(x)[1] - 1; precision = Float32), γ = 3) = begin
+(nn::BGKNet)(x, p, vs = VSpace1D(-6, 6, size(x)[1] - 1), γ = 3) = begin
     nm = param_length(nn.Mnet)
-    f = @view x[begin:end-1, :]
-    τ = @view x[end:end, :]
-    M = f_maxwellian(f, vs, γ)
-    y = f .- M
-    z = vcat(y, τ)
-    
-    (relu(M .+ nn.Mnet(y, p[1:nm])) .- f) ./ (τ .* (1 .+ 0.2 .* tanh.(nn.νnet(z, p[nm+1:end]))))
+
+    if size(x, 1) > vs.nu + 1
+        h = @view x[begin:vs.nu, :]
+        b = @view x[vs.nu+1:end-1, :]
+        f = @view x[begin:end-1, :]
+        τ = @view x[end:end, :]
+
+        H, B = f_maxwellian(h, b, vs, γ)
+        M = [H; B]
+        y = f .- M
+        z = vcat(y, τ)
+
+        return (relu(M .+ nn.Mnet(y, p[1:nm])) .- f) ./
+               (τ .* (1 .+ 0.9 .* elu.(nn.νnet(z, p[nm+1:end]))))
+    else
+        f = @view x[begin:end-1, :]
+        τ = @view x[end:end, :]
+        M = f_maxwellian(f, vs, γ)
+        y = f .- M
+        z = vcat(y, τ)
+
+        return (relu(M .+ nn.Mnet(y, p[1:nm])) .- f) ./
+               (τ .* (1 .+ 0.9 .* elu.(nn.νnet(z, p[nm+1:end]))))
+        #(τ .* (1 .+ 0.2 .* tanh.(nn.νnet(z, p[nm+1:end]))))
+    end
 end
 
 Solaris.init_params(nn::BGKNet) = vcat(init_params(nn.Mnet), init_params(nn.νnet))
--- test/test_layer.jl
+++ test/test_layer.jl
@@ -1,5 +1,6 @@
 vs = KitML.KitBase.VSpace1D(-5, 5, 16; precision = Float32)
 
+# full distribution
 mn = FnChain(FnDense(vs.nu, vs.nu, tanh; bias = false), FnDense(vs.nu, vs.nu; bias = false))
 νn = FnChain(
     FnDense(vs.nu + 1, vs.nu, tanh; bias = false),
@@ -8,4 +9,15 @@ mn = FnChain(FnDense(vs.nu, vs.nu, tanh; bias = false), FnDense(vs.nu, vs.nu; bi
 nn = BGKNet(mn, νn)
 p = init_params(nn)
 
-nn(rand(Float32, vs.nu + 1, 2), p, vs)
+nn(rand(Float32, vs.nu + 1, 2), p, vs, 3)
+
+# reduced distribution
+mn = FnChain(FnDense(vs.nu*2, vs.nu*2, tanh; bias = false), FnDense(vs.nu*2, vs.nu*2; bias = false))
+νn = FnChain(
+    FnDense(vs.nu*2 + 1, vs.nu*2+1, tanh; bias = false),
+    FnDense(vs.nu*2+1, vs.nu*2, relu; bias = false),
+)
+nn = BGKNet(mn, νn)
+p = init_params(nn)
+
+#nn(rand(Float32, vs.nu*2 + 1, 2), p, vs, 5/3)
