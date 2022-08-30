593855b5ff0730a27a589cad92a7e58682ba145d
vavrines <tianbaixiao@gmail.com>
2022-08-30 21:53:54 +0200

    Rewrite forward pass

--- src/Layer/layer.jl
+++ src/Layer/layer.jl
@@ -30,32 +30,42 @@ end
     vs = VSpace1D(-6, 6, size(x)[1] - 1),
     γ = ifelse(size(x, 1) > vs.nu + 1, 5 / 3, 3),
 ) = begin
+    if size(x, 1) > vs.nu + 1
+        return (nn)(x, p, vs, γ, VDF{2,1})
+    else
+        return (nn)(x, p, vs, γ, VDF{1,1})
+    end
+end
+
+(nn::BGKNet)(x, p, vs, γ, ::Type{VDF{1,1}}) = begin
     nm = param_length(nn.Mnet)
 
-    if size(x, 1) > vs.nu + 1
-        h = @view x[begin:vs.nu, :]
-        b = @view x[vs.nu+1:end-1, :]
-        f = @view x[begin:end-1, :]
-        τ = @view x[end:end, :]
+    f = @view x[begin:end-1, :]
+    τ = @view x[end:end, :]
+    M = f_maxwellian(f, vs, γ)
+    y = f .- M
+    z = vcat(y, τ)
 
-        H, B = f_maxwellian(h, b, vs, γ)
-        M = [H; B]
-        y = f .- M
-        z = vcat(y, τ)
+    return (relu(M .+ nn.Mnet(y, p[1:nm])) .- f) ./
+           (τ .* (1 .+ 0.9 .* elu.(nn.νnet(z, p[nm+1:end]))))
+    #(τ .* (1 .+ 0.2 .* tanh.(nn.νnet(z, p[nm+1:end]))))
+end
 
-        return (relu(M .+ nn.Mnet(y, p[1:nm])) .- f) ./
-               (τ .* (1 .+ 0.9 .* elu.(nn.νnet(z, p[nm+1:end]))))
-    else
-        f = @view x[begin:end-1, :]
-        τ = @view x[end:end, :]
-        M = f_maxwellian(f, vs, γ)
-        y = f .- M
-        z = vcat(y, τ)
+(nn::BGKNet)(x, p, vs, γ, ::Type{VDF{2,1}}) = begin
+    nm = param_length(nn.Mnet)
 
-        return (relu(M .+ nn.Mnet(y, p[1:nm])) .- f) ./
-               (τ .* (1 .+ 0.9 .* elu.(nn.νnet(z, p[nm+1:end]))))
-        #(τ .* (1 .+ 0.2 .* tanh.(nn.νnet(z, p[nm+1:end]))))
-    end
+    h = @view x[begin:vs.nu, :]
+    b = @view x[vs.nu+1:end-1, :]
+    f = @view x[begin:end-1, :]
+    τ = @view x[end:end, :]
+
+    H, B = f_maxwellian(h, b, vs, γ)
+    M = [H; B]
+    y = f .- M
+    z = vcat(y, τ)
+
+    return (relu(M .+ nn.Mnet(y, p[1:nm])) .- f) ./
+           (τ .* (1 .+ 0.9 .* elu.(nn.νnet(z, p[nm+1:end]))))
 end
 
 Solaris.init_params(nn::BGKNet) = vcat(init_params(nn.Mnet), init_params(nn.νnet))
