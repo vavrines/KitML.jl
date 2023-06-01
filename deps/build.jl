e30e84ac4c0b588de10b50f36202957b1592bcf0
vavrines <tianbaixiao@gmail.com>
2023-06-01 10:02:28 +0800

    Fix build

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
-using Conda
-Conda.add_channel("conda-forge")
-Conda.add("tensorflow")
+#using Conda
+#Conda.add_channel("conda-forge")
+#Conda.add("tensorflow")
 
 cmd = `pip3 install tensorflow --user`
 run(cmd)
