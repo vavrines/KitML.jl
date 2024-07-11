aeccad9a03616ba26fedbce59eac8ea6261e8889
Tianbai Xiao <tianbaixiao@gmail.com>
2024-07-11 15:43:12 +0200

    Format codes

--- test/test_layer.jl
+++ test/test_layer.jl
@@ -13,15 +13,18 @@ p = init_params(nn)
 nn(rand(Float32, vs.nu + 1, 2), p, vs, 3)
 
 # reduced distribution
-mn = FnChain(FnDense(vs.nu*2, vs.nu*2, tanh; bias = false), FnDense(vs.nu*2, vs.nu*2; bias = false))
+mn = FnChain(
+    FnDense(vs.nu * 2, vs.nu * 2, tanh; bias = false),
+    FnDense(vs.nu * 2, vs.nu * 2; bias = false),
+)
 νn = FnChain(
-    FnDense(vs.nu*2 + 1, vs.nu*2+1, tanh; bias = false),
-    FnDense(vs.nu*2+1, vs.nu*2, relu; bias = false),
+    FnDense(vs.nu * 2 + 1, vs.nu * 2 + 1, tanh; bias = false),
+    FnDense(vs.nu * 2 + 1, vs.nu * 2, relu; bias = false),
 )
 nn = BGKNet(mn, νn)
 p = init_params(nn)
 
-nn(rand(Float32, vs.nu*2 + 1, 2), p, vs, 5/3)
+nn(rand(Float32, vs.nu * 2 + 1, 2), p, vs, 5 / 3)
 
 #--- class 2 ---#
 # full distribution
@@ -36,12 +39,15 @@ p = init_params(nn)
 nn(rand(Float32, vs.nu + 1, 2), p, vs, 3, Class{2})
 
 # reduced distribution
-mn = FnChain(FnDense(vs.nu*2, vs.nu*2, tanh; bias = false), FnDense(vs.nu*2, 6; bias = false))
+mn = FnChain(
+    FnDense(vs.nu * 2, vs.nu * 2, tanh; bias = false),
+    FnDense(vs.nu * 2, 6; bias = false),
+)
 νn = FnChain(
-    FnDense(vs.nu*2 + 1, vs.nu*2+1, tanh; bias = false),
-    FnDense(vs.nu*2+1, vs.nu*2, relu; bias = false),
+    FnDense(vs.nu * 2 + 1, vs.nu * 2 + 1, tanh; bias = false),
+    FnDense(vs.nu * 2 + 1, vs.nu * 2, relu; bias = false),
 )
 nn = BGKNet(mn, νn)
 p = init_params(nn)
 
-nn(rand(Float32, vs.nu*2 + 1, 2), p, vs, 5/3, Class{2})
+nn(rand(Float32, vs.nu * 2 + 1, 2), p, vs, 5 / 3, Class{2})
