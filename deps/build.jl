71dd45be159a393a9eb86beb4721dfecdfde1346
vavrines <tianbaixiao@gmail.com>
2025-12-05 21:24:44 +0800

    Try stopping Conda

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,8 @@
 using Conda
 #Conda.add_channel("conda-forge")
 #Conda.add("tensorflow")
-Conda.pip_interop(true)
-Conda.pip("install", "tensorflow")
+#Conda.pip_interop(true, Conda.ROOTENV)
+#Conda.pip("install", "tensorflow")
 
 cmd = `pip3 install tensorflow --user`
 run(cmd)
